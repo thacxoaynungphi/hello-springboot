8d55be7b8f268063c3e3c0555230cd87f3250113
Dat Phan <dat.phan@vti.com.vn>
2018-05-03 23:32:35 +0700

    Initial commit

---
+++ src/main/java/com/example/datdemo/DatdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.datdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/datdemo/DatdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.datdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
